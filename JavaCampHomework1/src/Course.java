7cb36a3177d101fe4d9fdeb972ec9ea949b81f5d
erdinc-akdemir <77392569+erdinc-akdemir@users.noreply.github.com>
2021-05-01 11:43:16 +0300

    OOPIntro

---
+++ JavaCampHomework1/src/Course.java
@@ -0,0 +1,22 @@
+
+public class Course {
+	
+	int id;
+	double price;
+	String name;
+	String category;
+	Lecturer lecturer;
+	
+	public Course(int id,double price,String name,String category,Lecturer lecturer) {
+		
+		this.id=id;
+		this.price=price;
+		this.name=name;
+		this.category=category;
+		this.lecturer=lecturer;
+		
+	}
+	
+	
+
+}
---
+++ JavaCampHomework1/src/Lecturer.java
@@ -0,0 +1,13 @@
+
+public class Lecturer {
+
+	int id;
+	String name;
+	String skills;
+	
+	public Lecturer(int id,String name,String skills) {
+		this.id=id;
+		this.name=name;
+		this.skills=skills;
+	}
+}
